97165987a37977241dca8a3317b504df32aa9148
Ruthline <rutheveline@hotmail.es>
2022-06-09 07:57:40 -0500

    Portafolio Update

--- src/components/mainf/Mainf.js
+++ src/components/mainf/Mainf.js
@@ -1,16 +1,30 @@
 import '../mainf/Mainf.css';
-import Foto from '../mainf/foto.png'
+import Programer from '../mainf/programer.png';
+
 function Mainf(){
     return(
         <main className='containF'>
                 
                 <div className="textMe">
-                <h1>About Me</h1>
-                    <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+                    <div className='resquare'>
+                        <div className='square-up'>
+                            <h1>¿Por qué Desarrolladora?</h1>
+                            <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo </p>
+                        </div>
+                        <div className='square-middle'>
+                        <img src={Programer}></img>
+                        </div>
+                        <div className='square-down'>
+                            <h1>Area Frontend</h1>
+                        <p>consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+                        </div>
+                    </div>
                 </div>
-            <img src={Foto}></img>
+            
             
         </main>
     );
 }
 export default Mainf;
+
+
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,22 +1,31 @@
 import { Link } from 'react-router-dom';
 import'../navbar/Navbar.css';
+import Logo from '../navbar/Logo.png';
 function Navbar(){
     return(
         
-        <navbar className='navBar'>
-        <Link to="/home" className='item'>
-            <i class="fa-solid fa-house-chimney"></i>
-            Home
-        </Link>
-        <Link to="/projects" className='item'>
-        <i class="fa-solid fa-house-chimney"></i>
-        Projects
-        </Link>
-        <Link to="/studies" className='item'>
-        <i class="fa-solid fa-house-chimney"></i>
-        Studies
-        </Link>
-        </navbar>
+        <header>
+            <div className="header-index">
+                    
+                <div className="nav-group">
+                    
+                    <Link to="/home" className="one-nav"><img src={Logo}></img></Link>
+                    
+                    <label for="clic-menu" className="three-nav" href="meni" ><i className="fa-solid fa-bars"></i></label>
+                    <input type="checkbox" id="clic-menu"></input>
+                    
+
+                    
+                    <div className="items">
+                    <Link to="/home" className="four-nav item" >Home</Link>
+                    <Link to="/projects"  className="five-nav item">Proyectos</Link>
+                    <Link to="/studies" className="six-nav item">Estudios</Link>
+                    <Link to="/skills" className="seven-nav item">Habilidades</Link>
+                   
+                    </div>
+                </div>
+            </div>
+        </header>
     );
 }
 
